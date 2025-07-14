b1dba2ff60417408cfdd7c6e82b5eaa182ad6cbb
Faizan Parabtani <faizan11911@gmail.com>
2025-07-14 13:23:52 -0400

    Fixed render hostname variable and added build step to createsuperuser

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'portfoliosite.deployment_settings' if 'RENDER_HOSTNAME' in os.environ else 'portfoliosite.settings'
+    settings_module = 'portfoliosite.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'portfoliosite.settings'
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
     try:
         from django.core.management import execute_from_command_line
--- portfoliosite/asgi.py
+++ portfoliosite/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-settings_module = 'portfoliosite.deployment_settings' if 'RENDER_HOSTNAME' in os.environ else 'portfoliosite.settings'
+settings_module = 'portfoliosite.deployment_settings' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'portfoliosite.settings'
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
 application = get_asgi_application()
